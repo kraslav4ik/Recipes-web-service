20ee537e50e5db20dec4012823da04300a007c3b
slava <slavak540@gmail.com>
2022-11-05 18:16:29 +0100

    added dates and recipes categories, added search request

--- src/main/java/recipes/repositories/RecipesRepository.java
+++
@@ -1,9 +0,0 @@
-package recipes.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import recipes.services.Recipe;
-
-@Repository
-public interface RecipesRepository extends CrudRepository<Recipe, Long> {
-}
